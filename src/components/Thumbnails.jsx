9b7cb9e19ec1f42abbbc61adaa70f422e21d7e2c
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-06-05 18:59:32 +0100

    Updated: map array data, added click event

--- src/components/Thumbnails.jsx
+++ src/components/Thumbnails.jsx
@@ -1,3 +1,13 @@
-export const Thumbnails = () => {
-  return <section></section>;
+export const Thumbnails = ({ apiData, handleClick }) => {
+  const imgElements = apiData.map((element) => {
+    return (
+      <img
+        key={element.id}
+        src={element.urls.thumb}
+        alt={element.alt_description}
+        onClick={() => handleClick(element.id)}
+      />
+    );
+  });
+  return <section className="flex">{imgElements}</section>;
 };
