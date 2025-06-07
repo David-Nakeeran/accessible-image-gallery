ddd73a8d81f688014e999c406466eb2ad0e0448c
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-06-07 15:53:50 +0100

    Added: accessibility to thumbnails

--- src/components/Thumbnails.jsx
+++ src/components/Thumbnails.jsx
@@ -2,11 +2,14 @@ export const Thumbnails = ({ apiData, handleClick }) => {
   const imgElements = apiData.map((element) => {
     return (
       <img
-        className="w-[30%]"
+        className="w-[30%] focus:outline-2 focus:outline-offset-none focus:outline-[#d1b3ff]"
+        role="button"
+        tabIndex="0"
         key={element.id}
         src={element.urls.thumb}
         alt={element.alt_description}
         onClick={() => handleClick(element.id)}
+        onKeyDown={(e) => (e.key === "Enter" ? handleClick(element.id) : null)}
       />
     );
   });
